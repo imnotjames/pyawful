4459adf4964ee617be91f5bfe5528a222afeddaa
James Ward <james@notjam.es>
2024-12-11 10:41:15 -0500

    test: move fixtures to their own conftest

--- tests/parse/test_error_parser.py
+++ tests/parse/test_error_parser.py
@@ -1,7 +1,4 @@
-import os
-
-import pytest
-from lxml.html import fromstring
+from lxml.html import HtmlElement
 
 from pyawful.errors import (
     AwfulError,
@@ -12,50 +9,8 @@ from pyawful.errors import (
 from pyawful.parse.error_parser import parse_error
 
 
-def get_example(filename: str) -> str:
-    fixture_file = os.path.join(os.path.dirname(__file__), "fixtures", filename)
-
-    with open(fixture_file) as f:
-        return f.read()
-
-
-@pytest.fixture()
-def example_no_error_html():
-    return get_example("member.html")
-
-
-@pytest.fixture()
-def example_bad_page_html():
-    return get_example("error_bad_page.html")
-
-
-@pytest.fixture()
-def example_bad_forum_html():
-    return get_example("error_bad_forum.html")
-
-
-@pytest.fixture()
-def example_requires_auth_html():
-    return get_example("error_requires_auth.html")
-
-
-@pytest.fixture()
-def example_bad_password_html():
-    return get_example("error_bad_password.html")
-
-
-@pytest.fixture()
-def example_logout_page_html():
-    return get_example("logout_page.html")
-
-
-@pytest.fixture()
-def example_login_page_html():
-    return get_example("login_page.html")
-
-
-def test_parse_gets_bad_page_error(example_bad_page_html: str):
-    response = parse_error(fromstring(example_bad_page_html))
+def test_parse_gets_bad_page_error(example_bad_page: HtmlElement):
+    response = parse_error(example_bad_page)
 
     assert type(response) is AwfulError
     assert (
@@ -63,39 +18,39 @@ def test_parse_gets_bad_page_error(example_bad_page_html: str):
     )
 
 
-def test_parse_gets_bad_forum_error(example_bad_forum_html: str):
-    response = parse_error(fromstring(example_bad_forum_html))
+def test_parse_gets_bad_forum_error(example_bad_forum: HtmlElement):
+    response = parse_error(example_bad_forum)
 
     assert type(response) is AwfulError
     assert str(response) == "Specified forum was not found in the live forums."
 
 
-def test_parse_gets_requires_auth_error(example_requires_auth_html: str):
-    response = parse_error(fromstring(example_requires_auth_html))
+def test_parse_gets_requires_auth_error(example_requires_auth: HtmlElement):
+    response = parse_error(example_requires_auth)
 
     assert isinstance(response, RequiresAuthorization)
 
 
-def test_parse_gets_bad_password_error(example_bad_password_html: str):
-    response = parse_error(fromstring(example_bad_password_html))
+def test_parse_gets_bad_password_error(example_bad_password: HtmlElement):
+    response = parse_error(example_bad_password)
 
     assert isinstance(response, InvalidLoginCredentials)
     assert str(response) == "BAD PASSWORD!"
     assert response.attempt_count == 1
 
 
-def test_parse_gets_logout_error(example_logout_page_html: str):
-    response = parse_error(fromstring(example_logout_page_html))
+def test_parse_gets_logout_error(example_logout_page: HtmlElement):
+    response = parse_error(example_logout_page)
     assert isinstance(response, WasLoggedOut)
 
 
-def test_parse_gets_no_error_on_normal_page(example_no_error_html: str):
-    response = parse_error(fromstring(example_no_error_html))
+def test_parse_gets_no_error_on_normal_page(example_no_error: HtmlElement):
+    response = parse_error(example_no_error)
 
     assert response is None
 
 
-def test_parse_gets_no_error_on_login_page(example_login_page_html: str):
-    response = parse_error(fromstring(example_login_page_html))
+def test_parse_gets_no_error_on_login_page(example_login_page: HtmlElement):
+    response = parse_error(example_login_page)
 
     assert response is None
--- tests/parse/test_forum_parser.py
+++ tests/parse/test_forum_parser.py
@@ -1,31 +1,17 @@
-import os
-
-import pytest
-from lxml.html import fromstring
+from lxml.html import HtmlElement
 
 from pyawful.parse.forum_parser import parse_forum_page
 
 
-@pytest.fixture()
-def example_html():
-    fixture_file = os.path.join(
-        os.path.dirname(__file__),
-        "fixtures/forumdisplay.html",
-    )
-
-    with open(fixture_file) as f:
-        yield f.read()
-
-
-def test_parse_gets_page_location(example_html: str):
-    response = parse_forum_page(fromstring(example_html))
+def test_parse_gets_page_location(example_forum_display: HtmlElement):
+    response = parse_forum_page(example_forum_display)
 
     assert response.current_page == 1
     assert response.last_page == 651
 
 
-def test_parse_gets_forum_name(example_html: str):
-    response = parse_forum_page(fromstring(example_html))
+def test_parse_gets_forum_name(example_forum_display: HtmlElement):
+    response = parse_forum_page(example_forum_display)
 
     assert response.forum.id == 273
     assert response.forum.name == "General Bullshit"
